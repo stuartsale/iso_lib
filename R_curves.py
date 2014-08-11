3b63cc6ae8502518c789f852c9ff6b7ed4e23fbc
Stuart Sale <stuart.sale@gmail.com>
2014-08-11 15:16:33 +0000

    Given correct filenames for R-curves

--- R_curves.py
+++ R_curves.py
@@ -15,7 +15,7 @@ class R_curves:
         self.vha_splines=[]
         
         for R in np.arange(2.1, 5.1, 0.1):
-            R_file=np.genfromtxt("{0:s}/31.2out".format(directory,int(R*10)), usecols=(1,13,14,15,16,17,18) )[::-1]
+            R_file=np.genfromtxt("{0:s}/{1:d}.2out".format(directory,int(R*10)), usecols=(1,13,14,15,16,17,18) )[::-1]
             
             self.ur_splines.append(si.UnivariateSpline(np.log10(R_file[:,0]),R_file[:,2],k=1) )
             self.vr_splines.append(si.UnivariateSpline(np.log10(R_file[:,0]),R_file[:,1],k=1) )
