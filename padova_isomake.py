c8d92d7bac81374b7a71e4b26815e1cda675ae92
Stuart Sale <stuart.sale@gmail.com>
2014-10-21 14:03:23 +0000

    getting basic function to read in padova isochrones working

--- padova_isomake.py
+++ padova_isomake.py
@@ -8,7 +8,7 @@ import glob as gb
 
 
 
-def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
+def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict, bands_ordered):
     print metallicity, metal_weight
 
     logT_min=3.3
@@ -28,7 +28,7 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
     # Find columns required
     
     with open(filename, 'r') as f:
-        for x in (100)
+        for x in range(100):
             header_line=f.readline().split()
             if "M_ini" in header_line:
                 break    
@@ -42,12 +42,16 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
     
     for band in bands_dict.keys():
         if band in header_line:
-            photom_cols[bands_dict[band] ] =  = header_line.index(band)-1
+            photom_cols[bands_dict[band] ] = header_line.index(band)-1
         
 
     # Read in data - need Z, age, Mi, logT, logg, r, i, Ha
 
-    iso_data=np.loadtxt( filename, usecols=[Mi_col, logage_col, logTe_col, logg_col].extend(photom_cols.values() ) )
+    iso_data=np.loadtxt( filename, usecols=(Mi_col, logage_col, logTe_col, logg_col) )
+    
+    photom_data={}
+    for band in photom_cols.keys():
+        photom_data[band]=np.loadtxt( filename, usecols=[photom_cols[band]] )
 
     # Set metallicity
 
@@ -61,9 +65,9 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
             weights[i]=(iso_data[i+1,0]-iso_data[i,0])*np.power(10,iso_data[i,1])
         elif i==iso_data.shape[0]-1:
             weights[i]=(iso_data[i,0]-iso_data[i-1,0])*np.power(10,iso_data[i,1])
-        elif iso_data[i,2]>iso_data[i+1,2]:
+        elif iso_data[i,0]>iso_data[i+1,0]:
             weights[i]=(iso_data[i,0]-iso_data[i-1,0])*np.power(10,iso_data[i,1])
-        elif iso_data[i-1,2]>iso_data[i,2]:
+        elif iso_data[i-1,0]>iso_data[i,0]:
             weights[i]=(iso_data[i+1,0]-iso_data[i,0])*np.power(10,iso_data[i,1])
         else:
             weights[i]=(iso_data[i+1,0]-iso_data[i-1,0])*np.power(10,iso_data[i,1])
@@ -73,14 +77,14 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
 
     # Index data
 
-    binned_data=[ [ [] for i in range(13) ] for i in range(14) ]
+    binned_data=[ [ [] for i in range(14) ] for i in range(14) ]
 
     for i in range(iso_data.shape[0]):
 
         try:
-            binned_data[int(np.floor((iso_data[i,3]-3.3)/0.1))][int(np.floor((iso_data[i,4]+1.5)/0.5))].append(iso_data[:])
+            binned_data[int(np.floor((iso_data[i,2]-3.3)/0.1))][int(np.floor((iso_data[i,3]+1.5)/0.5))].append(iso_data[:])
         except IndexError:
-            print datum[3], datum[4] 
+            print "Error:", iso_data[i,2], iso_data[i,3]
 
 
 
@@ -105,43 +109,60 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
     Mi=np.zeros(interp_points.T.shape[0])
     
     interp_photom={}
-    for band in bands_dict.values():
+    for band in photom_data.keys():
         interp_photom[band] = np.zeros(interp_points.T.shape[0])
     
     for it, point in enumerate(interp_points.T):
+        if it%10000==0:
+            print it, interp_points.shape[1]
 #        print point, int(np.floor((point[0]-3.4)/0.1)), int(np.floor((point[1]+0.5)/0.5))
 #        iso_list=binned_data[int(np.floor((point[0]-3.4)/0.1))][int(np.floor((point[1]+0.5)/0.5))]
 
-        shortlist=iso_data[np.power(iso_data[:,3]-point[0],2)*4. + np.power(iso_data[:,4]-point[1],2)< 0.01]
-        if shortlist.size==0:
+        selection_array=np.power(iso_data[:,2]-point[0],2)*4. + np.power(iso_data[:,3]-point[1],2)< 0.01
+
+        shortlist_iso=iso_data[selection_array]
+        shortlist_weights=weights[selection_array]
+        shortlist_photom={}
+        for band in photom_data.keys():
+            shortlist_photom[band]=photom_data[band][selection_array]
+        
+        if shortlist_iso.size==0:
             logage[it]=np.nan
             Mi[it]=np.nan
 
-             for band in bands_dict.values():
+            for band in bands_dict.values():
                 interp_photom[band][it] = np.nan        
 
         else:
-            short_weights1=shortlist[:,-1]/(1E-9+np.power(shortlist[:,3]-point[0],2.)*36.+np.power(shortlist[:,4]-point[1],2.))
+            short_weights1=shortlist_weights[:,-1]/(1E-9+np.power(shortlist_iso[:,2]-point[0],2.)*36.+np.power(shortlist_iso[:,3]-point[1],2.))
             short_weights_sum1=np.sum(short_weights1)
-            short_weights2=1/(1E-9+np.power(shortlist[:,3]-point[0],2.)*36.+np.power(shortlist[:,4]-point[1],2.))
+            short_weights2=1/(1E-9+np.power(shortlist_iso[:,2]-point[0],2.)*36.+np.power(shortlist_iso[:,3]-point[1],2.))
             short_weights_sum2=np.sum(short_weights2)
 
-            logage[it]=np.sum(shortlist[:,1]*short_weights1)/short_weights_sum1
-            Mi[it]=np.sum(shortlist[:,2]*short_weights1)/short_weights_sum1
-            r[it]=np.sum(shortlist[:,5]*short_weights2)/short_weights_sum2
-            i[it]=np.sum(shortlist[:,6]*short_weights2)/short_weights_sum2
-            ha[it]=np.sum(shortlist[:,7]*short_weights2)/short_weights_sum2
-    
-    inner_counts, xe, ye=np.histogram2d(iso_data[:,3], iso_data[:,4], bins=[logT_edges, logg_edges])
+            logage[it]=np.sum(shortlist_iso[:,1]*short_weights1)/short_weights_sum1
+            Mi[it]=np.sum(shortlist_iso[:,0]*short_weights1)/short_weights_sum1
+            
+            for band in bands_dict.values():
+                interp_photom[band][it]=np.sum(shortlist_photom[band]*short_weights2)/short_weights_sum2
+                
+    inner_counts, xe, ye=np.histogram2d(iso_data[:,2], iso_data[:,3], bins=[logT_edges, logg_edges])
     outer_counts=(snd.filters.uniform_filter(inner_counts, size=(2,5))*25+0.1).astype(int)
-    inner_Jac, xe, ye=np.histogram2d(iso_data[:,3], iso_data[:,4], bins=[logT_edges, logg_edges], weights=weights1)
+    inner_Jac, xe, ye=np.histogram2d(iso_data[:,2], iso_data[:,3], bins=[logT_edges, logg_edges], weights=weights1)
     outer_Jac=(snd.filters.uniform_filter(inner_Jac, size=(2,5))*25)
 
     outer_Jac[outer_Jac<0.01]=0.
     
     # Save to file
+    
+    output_array=[metals, Mi, logage, interp_points[0], interp_points[1], outer_Jac.flatten()]
+    fmt_list=['%.3f', '%.3f', '%.3f', '%.3f', '%.3f', '%.3e']
+    for band in bands_ordered:
+        output_array.appendinterp_photom[band]
+        fmt_list.append('%.3f')
+    output_array.extend([inner_counts.flatten(), outer_counts.flatten()])
+    fmt_list.extend(['%i', '%i'])    
 
-    np.savetxt(output_obj, np.array([metals, Mi, logage, interp_points[0], interp_points[1], outer_Jac.flatten(), r,i,ha, inner_counts.flatten(), outer_counts.flatten() ]).T, fmt=['%.3f', '%.3f', '%.3f', '%.3f', '%.3f', '%.3e', '%.3f', '%.3f', '%.3f', '%i', '%i'])
+    np.savetxt(output_obj, np.array(output_array).T, fmt=fmt_list)
     
     
     
@@ -149,4 +170,4 @@ def iso_interp(filename, metallicity, metal_weight, output_obj, bands_dict):
     
     
     
-def padova_interpolated_isomake(folders, bands, output_file):
+#def padova_interpolated_isomake(folders, bands, output_file):
