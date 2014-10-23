24f0db5f91a6a44f2df6453d3edcda7061e5a986
Stuart Sale <stuart.sale@gmail.com>
2014-10-23 13:06:15 +0000

    bug fixes

--- iso_grid_tg.py
+++ iso_grid_tg.py
@@ -8,7 +8,7 @@ class iso_grid_tefflogg:
 		- set up to work in {Teff, logg, feh} space
 		- Asumes a regular grid in '''
 
-	def __init__(self, filename, metal_col=0, Mi_col=1, logage_col=2, teff_col=3, logg_col=4, Jac_col=5, bands, verbose=False):
+	def __init__(self, filename, metal_col=0, Mi_col=1, logage_col=2, teff_col=3, logg_col=4, Jac_col=5, bands=None, verbose=False):
 		self.metal_col=metal_col
 		self.teff_col=teff_col
 		self.logg_col=logg_col
@@ -53,7 +53,7 @@ class iso_grid_tefflogg:
 		    
 
 		self.iso_array2=io.iso_objs(iso_array[:,Mi_col], iso_array[:,logage_col], iso_array[:,metal_col], iso_array[:,teff_col], iso_array[:,logg_col],
-						 abs_mags, iso_array[:,Jac_col])
+						 abs_mags, iso_array[:,Jac_col], bands=bands)
 		
 
 	def register(self, array):
--- iso_obj.py
+++ iso_obj.py
@@ -4,80 +4,80 @@ import math
 from R_curves import R_curves
 
 class iso_objs:
-	""" As iso_obj, but intended to hold multiple objects [e.g. an entire library, an isochrone]"""
+    """ As iso_obj, but intended to hold multiple objects [e.g. an entire library, an isochrone]"""
 
-	def __init__(self, Mi_in, logage_in, feh_in, logT_in, logg_in, abs_mag_in,
-        	Jac_in=None, AX1_in=None, AX2_in=None,
-			log_IMF_prob_in=None, log_SFR_prob_in=None, R_set=None, bands=None) :#R_curves("/home/sale/work-oxford/tracks/Phoenix")
-			
+    def __init__(self, Mi_in, logage_in, feh_in, logT_in, logg_in, abs_mag_in,
+            Jac_in=None, AX1_in=None, AX2_in=None,
+            log_IMF_prob_in=None, log_SFR_prob_in=None, R_set=None, bands=None) :#R_curves("/home/sale/work-oxford/tracks/Phoenix")
+            
         if  AX1_in is None and AX2_in is None:
             if bands is not None:
                 R_set=R_curves("/home/sale/work-oxford/tracks/Phoenix", bands)
             else:            
                 raise ValueError("Either the bands needed must be given or extinction coefficiants provided")
 
-#		if Mi_in.size!=logage_in or Mi_in.size!=feh_in or Mi_in.size!=logT_in or Mi_in.size!=logg_in or Mi_in.size!=r0_in or Mi_in.size!=i0_in or Mi_in.size!=ha0_in:
-#			raise TypeError("Mismatched Input Arrays")
+#        if Mi_in.size!=logage_in or Mi_in.size!=feh_in or Mi_in.size!=logT_in or Mi_in.size!=logg_in or Mi_in.size!=r0_in or Mi_in.size!=i0_in or Mi_in.size!=ha0_in:
+#            raise TypeError("Mismatched Input Arrays")
 
-		self.Mi=Mi_in
-		self.logage=logage_in
-		self.feh=feh_in
+        self.Mi=Mi_in
+        self.logage=logage_in
+        self.feh=feh_in
 
-		self.logT=logT_in
-		self.logg=logg_in
-		
-		self.abs_mag=abs_mag_in
+        self.logT=logT_in
+        self.logg=logg_in
+        
+        self.abs_mag=abs_mag_in
 
-		if Jac_in is not None:
-			self.Jac=Jac_in
-		else:
-			self.Jac=np.ones(self.Mi.shape)
-			
-		if AX1_in is None:
-		    self.AX1={}
-		    print "splnes: ", R_set.A1_splines.keys()
-		    for band in bands:
-		        self.AX1[band]=np.array([ R(self.logT) for R in R_set.A1_splines[band] ]).T
-		else:
-		    self.AX1=AX1_in
-		    
-		if AX2_in is None:
-		    self.AX2={}
-		    for band in bands:
-		        self.AX2[band]=np.array([ R(self.logT) for R in R_set.A2_splines[band] ]).T
-		else:
-		    self.AX2=AX2_in		    
+        if Jac_in is not None:
+            self.Jac=Jac_in
+        else:
+            self.Jac=np.ones(self.Mi.shape)
+            
+        if AX1_in is None:
+            self.AX1={}
+            print "splines: ", R_set.A1_splines.keys()
+            for band in bands:
+                self.AX1[band]=np.array([ R(self.logT) for R in R_set.A1_splines[band] ]).T
+        else:
+            self.AX1=AX1_in
+            
+        if AX2_in is None:
+            self.AX2={}
+            for band in bands:
+                self.AX2[band]=np.array([ R(self.logT) for R in R_set.A2_splines[band] ]).T
+        else:
+            self.AX2=AX2_in            
 
-		if log_IMF_prob_in is None:
-			self.log_IMF_prob=-2.7*self.Mi
-		else:
-			self.log_IMF_prob=log_IMF_prob_in
+        if log_IMF_prob_in is None:
+            self.log_IMF_prob=-2.7*self.Mi
+        else:
+            self.log_IMF_prob=log_IMF_prob_in
 
-		if log_SFR_prob_in is None:
-			self.log_SFR_prob=np.zeros(self.Mi.shape)
-		else:
-			self.log_SFR_prob=log_SFR_prob_in	
-			
-	def AX(self, band, A0, R=3.1):
-		index=np.rint((R-2.1)/0.1)
-		return self.AX2[band][index]*A0*A0 + self.AX1[band][index]*A0			
+        if log_SFR_prob_in is None:
+            self.log_SFR_prob=np.zeros(self.Mi.shape)
+        else:
+            self.log_SFR_prob=log_SFR_prob_in    
+            
+    def AX(self, band, A0, R=3.1):
+        index=np.rint((R-2.1)/0.1)
+        return self.AX2[band][index]*A0*A0 + self.AX1[band][index]*A0            
     
 
-	def redline(self, r_i1):
-		A_int=(-(self.abs_mag["r"]-self.abs_mag["i"])+r_i1)/(self.Ar1-self.Ai1)
-		return (self.Ar2-self.Aha2)*pow(A_int,2) + (self.Ar1-self.Aha2)*A_int + (self.abs_mag["r"]-self.abs_mag["Ha"])
+    def redline(self, r_i1):
+        A_int=(-(self.abs_mag["r"]-self.abs_mag["i"])+r_i1)/(self.Ar1-self.Ai1)
+        return (self.Ar2-self.Aha2)*pow(A_int,2) + (self.Ar1-self.Aha2)*A_int + (self.abs_mag["r"]-self.abs_mag["Ha"])
 
-	def subset(self, lines):	# Copy certain rows into a new iso_objs instance
-		AX1_cut={}
-		AX2_cut={}
-		abs_mag_cut={}
+    def subset(self, lines):    # Copy certain rows into a new iso_objs instance
+        AX1_cut={}
+        AX2_cut={}
+        abs_mag_cut={}
 
-		for band in self.abs_mag.keys():
-			AX1_cut[band]=self.AX1[band][lines]
-			AX2_cut[band]=self.AX2[band][lines]
-			abs_mag_cut[band]=self.abs_mag[band][lines]
-	    
-		return iso_objs(self.Mi[lines], self.logage[lines], self.feh[lines], self.logT[lines], self.logg[lines], abs_mag_cut, self.Jac[lines], 
-		        AX1_cut, AX2_cut, self.log_IMF_prob[lines], self.log_SFR_prob[lines])
+        for band in self.abs_mag.keys():
+            AX1_cut[band]=self.AX1[band][lines]
+            AX2_cut[band]=self.AX2[band][lines]
+            abs_mag_cut[band]=self.abs_mag[band][lines]
+        
+        return iso_objs(self.Mi[lines], self.logage[lines], self.feh[lines], self.logT[lines], self.logg[lines], abs_mag_cut, self.Jac[lines], 
+                AX1_cut, AX2_cut, self.log_IMF_prob[lines], self.log_SFR_prob[lines])
 
 
