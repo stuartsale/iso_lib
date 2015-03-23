48bec9f4a91d4d55af69c137e372954762fd1a05
Stuart Sale <stuart.sale@gmail.com>
2015-03-23 11:36:18 +0000

    tabs -> spaces

--- iso_grid.py
+++ iso_grid.py
@@ -3,68 +3,68 @@ import math
 import scipy.interpolate as si
 
 class iso_grid:
-	'''A class which allows isochrones to be queried for many objects at the same time'''
+    '''A class which allows isochrones to be queried for many objects at the same time'''
 
-	def __init__(self, filename, metal_col=0, age_col=1, mass_col=2, regular_age_step=False):
-		self.metal_col=metal_col
-		self.age_col=age_col
-		self.mass_col=mass_col
+    def __init__(self, filename, metal_col=0, age_col=1, mass_col=2, regular_age_step=False):
+        self.metal_col=metal_col
+        self.age_col=age_col
+        self.mass_col=mass_col
 
-		self.regular_age_step=regular_age_step
+        self.regular_age_step=regular_age_step
 
-		self.iso_array=np.loadtxt(filename)
-		self.metal_dict={}
+        self.iso_array=np.loadtxt(filename)
+        self.metal_dict={}
 
-		self.metal_interp=None
-
-
-		if self.regular_age_step:
-			self.age_step=0
-			self.age_min=0
-			self.age_gridlen=0
-		else:
-			self.age_dict={}
-			self.age_interp=None
-
-		self.mass_dict={}
-		self.mass_interp=None
-
-		self.register(self.iso_array)
-
-	def register(self, array):
-
-		for i in range(array.shape[0]):
-			if array[i,self.age_col] not in self.metal_dict:
-				self.metal_dict[ array[i,self.metal_col] ]=i
+        self.metal_interp=None
 
 
-		self.metal_interp=si.interp1d(sorted(self.metal_dict.keys()), sorted(self.metal_dict.values()), kind='nearest', bounds_error=False)
+        if self.regular_age_step:
+            self.age_step=0
+            self.age_min=0
+            self.age_gridlen=0
+        else:
+            self.age_dict={}
+            self.age_interp=None
 
-		if self.regular_age_step:
-			self.age_min=array[0,self.age_col]
-			for i in range(sorted(self.metal_dict.values())[1]):
-				if array[i,self.age_col]!=self.age_min:
-					self.age_step=array[i,self.age_col]-self.age_min
-					self.age_gridlen=i
-					break
-			print self.age_min, self.age_step, self.age_gridlen
+        self.mass_dict={}
+        self.mass_interp=None
 
-		else:
-			for i in range(sorted(self.metal_dict.values()) ):
-				if array[i,self.age_col] not in self.age_dict:
-					self.age_dict[ array[i,self.age_col] ]=i
-			self.age_interp=si.interp1d(sorted(self.age_dict.keys()), sorted(self.age_dict.values()), kind='nearest', bounds_error=False)
+        self.register(self.iso_array)
 
+    def register(self, array):
 
-		for i in range(self.age_gridlen):
-			if array[i,self.mass_col] not in self.mass_dict:
-				self.mass_dict[ array[i,self.mass_col] ]=i
+        for i in range(array.shape[0]):
+            if array[i,self.age_col] not in self.metal_dict:
+                self.metal_dict[ array[i,self.metal_col] ]=i
 
 
-	def query(self, feh, Mi, age):
+        self.metal_interp=si.interp1d(sorted(self.metal_dict.keys()), sorted(self.metal_dict.values()), kind='nearest', bounds_error=False)
 
-		r=np.zeros(feh.shape)+4.1
-		i=np.zeros(feh.shape)+3.9
-		ha=np.zeros(feh.shape)+3.81
+        if self.regular_age_step:
+            self.age_min=array[0,self.age_col]
+            for i in range(sorted(self.metal_dict.values())[1]):
+                if array[i,self.age_col]!=self.age_min:
+                    self.age_step=array[i,self.age_col]-self.age_min
+                    self.age_gridlen=i
+                    break
+            print self.age_min, self.age_step, self.age_gridlen
 
-		return r, i, ha
+        else:
+            for i in range(sorted(self.metal_dict.values()) ):
+                if array[i,self.age_col] not in self.age_dict:
+                    self.age_dict[ array[i,self.age_col] ]=i
+            self.age_interp=si.interp1d(sorted(self.age_dict.keys()), sorted(self.age_dict.values()), kind='nearest', bounds_error=False)
+
+
+        for i in range(self.age_gridlen):
+            if array[i,self.mass_col] not in self.mass_dict:
+                self.mass_dict[ array[i,self.mass_col] ]=i
+
+
+    def query(self, feh, Mi, age):
+
+        r=np.zeros(feh.shape)+4.1
+        i=np.zeros(feh.shape)+3.9
+        ha=np.zeros(feh.shape)+3.81
+
+        return r, i, ha
--- iso_grid_tg.py
+++ iso_grid_tg.py
@@ -4,106 +4,106 @@ import scipy.interpolate as si
 import iso_obj as io
 
 class iso_grid_tefflogg:
-	'''A class which allows isochrones to be queried for many objects at the same time
-		- set up to work in {Teff, logg, feh} space
-		- Asumes a regular grid in '''
+    '''A class which allows isochrones to be queried for many objects at the same time
+        - set up to work in {Teff, logg, feh} space
+        - Asumes a regular grid in '''
 
-	def __init__(self, filename, metal_col=0, Mi_col=1, logage_col=2, teff_col=3, logg_col=4, Jac_col=5, bands=None, verbose=False):
-		self.metal_col=metal_col
-		self.teff_col=teff_col
-		self.logg_col=logg_col
-		
-		self.verbose=verbose
+    def __init__(self, filename, metal_col=0, Mi_col=1, logage_col=2, teff_col=3, logg_col=4, Jac_col=5, bands=None, verbose=False):
+        self.metal_col=metal_col
+        self.teff_col=teff_col
+        self.logg_col=logg_col
+        
+        self.verbose=verbose
 
 
-		iso_array=np.loadtxt(filename)
-		self.metal_dict={}
+        iso_array=np.loadtxt(filename)
+        self.metal_dict={}
 
-		self.metal_interp=None
+        self.metal_interp=None
 
-		self.teff_step=0.
-		self.teff_min=0.
-		self.teff_gridlen=0.
+        self.teff_step=0.
+        self.teff_min=0.
+        self.teff_gridlen=0.
 
-		self.logg_step=0.
-		self.logg_min=0.
-		self.logg_gridlen=0.
+        self.logg_step=0.
+        self.logg_min=0.
+        self.logg_gridlen=0.
 
-		self.register(iso_array)
+        self.register(iso_array)
 
-		abs_mags={}		
-		with open(filename, 'r') as f:
-			first_line = f.readline().split()
+        abs_mags={}        
+        with open(filename, 'r') as f:
+            first_line = f.readline().split()
             
-		for band in bands:
-			abs_mags[band]=iso_array[:,first_line.index(band)-1]
-			
-		if metal_col is not None:
-		    metal_col=first_line.index("[M/H]")-1	
-		if Mi_col is not None:
-		    Mi_col=first_line.index("Mi")-1
-		if logage_col is not None:
-		    logage_col=first_line.index("logAge")-1
-		if teff_col is not None:
-		    metal_col=first_line.index("logTe")-1
-		if logg_col is not None:
-		    metal_col=first_line.index("logg")-1
-		if Jac_col is not None:
-		    metal_col=first_line.index("Jacobian")-1		    		    		    		    
-		    
+        for band in bands:
+            abs_mags[band]=iso_array[:,first_line.index(band)-1]
+            
+        if metal_col is not None:
+            metal_col=first_line.index("[M/H]")-1    
+        if Mi_col is not None:
+            Mi_col=first_line.index("Mi")-1
+        if logage_col is not None:
+            logage_col=first_line.index("logAge")-1
+        if teff_col is not None:
+            metal_col=first_line.index("logTe")-1
+        if logg_col is not None:
+            metal_col=first_line.index("logg")-1
+        if Jac_col is not None:
+            metal_col=first_line.index("Jacobian")-1                                                
+            
 
-		self.iso_array2=io.iso_objs(iso_array[:,Mi_col], iso_array[:,logage_col], iso_array[:,metal_col], iso_array[:,teff_col], iso_array[:,logg_col],
-						 abs_mags, iso_array[:,Jac_col], bands=bands)
-		
+        self.iso_array2=io.iso_objs(iso_array[:,Mi_col], iso_array[:,logage_col], iso_array[:,metal_col], iso_array[:,teff_col], iso_array[:,logg_col],
+                         abs_mags, iso_array[:,Jac_col], bands=bands)
+        
 
-	def register(self, array):
+    def register(self, array):
 
-		if self.verbose:
-			print "Registering"
+        if self.verbose:
+            print "Registering"
 
-		for i in range(array.shape[0]):
+        for i in range(array.shape[0]):
 
-			if array[i,self.metal_col] not in self.metal_dict:
-				self.metal_dict[ array[i,self.metal_col] ]=i
+            if array[i,self.metal_col] not in self.metal_dict:
+                self.metal_dict[ array[i,self.metal_col] ]=i
 
-		if self.verbose:
-			print "metal dict:", self.metal_dict
+        if self.verbose:
+            print "metal dict:", self.metal_dict
 
-		self.metal_interp=si.interp1d(sorted(self.metal_dict.keys()), sorted(self.metal_dict.values()), kind='nearest', bounds_error=False)
-
-
-		self.teff_min=array[0,self.teff_col]
-		for i in range(sorted(self.metal_dict.values())[1]):
-			if array[i,self.teff_col]!=self.teff_min:
-				self.teff_step=array[i,self.teff_col]-self.teff_min
-				self.teff_gridlen=i
-				break
-		if self.verbose:
-			print "Teff grid:", self.teff_min, self.teff_step, self.teff_gridlen
-
-	
-		self.logg_min=array[0,self.logg_col]
-		for i in range(self.teff_gridlen):
-			if array[i,self.logg_col]!=self.logg_min:
-				self.logg_step=array[i,self.logg_col]-self.logg_min
-				self.logg_gridlen=i
-				break
-		if self.verbose:
-			print "logg grid:", self.logg_min, self.logg_step, self.logg_gridlen
-			
-		self.teff_max=np.max(array[:,self.teff_col])
-		self.logg_max=np.max(array[:,self.logg_col])				
-	
+        self.metal_interp=si.interp1d(sorted(self.metal_dict.keys()), sorted(self.metal_dict.values()), kind='nearest', bounds_error=False)
 
 
-	def query(self, feh, teff, logg):
-	
-		if np.any(teff>self.teff_max) or np.any(teff<self.teff_min) or np.any(logg>self.logg_max) or np.any(logg<self.logg_min):
-			raise IndexError()
+        self.teff_min=array[0,self.teff_col]
+        for i in range(sorted(self.metal_dict.values())[1]):
+            if array[i,self.teff_col]!=self.teff_min:
+                self.teff_step=array[i,self.teff_col]-self.teff_min
+                self.teff_gridlen=i
+                break
+        if self.verbose:
+            print "Teff grid:", self.teff_min, self.teff_step, self.teff_gridlen
 
-		rows=(self.metal_interp(feh) 
-			+ ( np.rint((teff-self.teff_min)/self.teff_step)*self.teff_gridlen ) 
-			+ ( np.rint((logg-self.logg_min)/self.logg_step)*self.logg_gridlen )).astype(int)
-			
-		return self.iso_array2.subset(rows)
-		
+    
+        self.logg_min=array[0,self.logg_col]
+        for i in range(self.teff_gridlen):
+            if array[i,self.logg_col]!=self.logg_min:
+                self.logg_step=array[i,self.logg_col]-self.logg_min
+                self.logg_gridlen=i
+                break
+        if self.verbose:
+            print "logg grid:", self.logg_min, self.logg_step, self.logg_gridlen
+            
+        self.teff_max=np.max(array[:,self.teff_col])
+        self.logg_max=np.max(array[:,self.logg_col])                
+    
+
+
+    def query(self, feh, teff, logg):
+    
+        if np.any(teff>self.teff_max) or np.any(teff<self.teff_min) or np.any(logg>self.logg_max) or np.any(logg<self.logg_min):
+            raise IndexError()
+
+        rows=(self.metal_interp(feh) 
+            + ( np.rint((teff-self.teff_min)/self.teff_step)*self.teff_gridlen ) 
+            + ( np.rint((logg-self.logg_min)/self.logg_step)*self.logg_gridlen )).astype(int)
+            
+        return self.iso_array2.subset(rows)
+        
